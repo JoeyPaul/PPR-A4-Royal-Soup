f8dae926500531e59a02f5ab5df5b60016f5b46a
JoeyPaul <joe.kallman@gmail.com>
2023-07-27 15:05:19 +1000

    Camera Activation
    Controls have been added with mouse click

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,8 @@ public class GameController : MonoBehaviour
 {
     [SerializeField]
     TurnBasedPlayerMovement player;
+    [SerializeField]
+    GameObject freeLookCamera;
 
     // Update is called once per frame
     void Update()
@@ -13,6 +15,14 @@ public class GameController : MonoBehaviour
         if (player.arrivedAtKing) 
         {
             print("You reached the king!");
+        }
+        if (Input.GetMouseButtonDown(0)) // Mouse 1 is pressed
+        {
+            freeLookCamera.SetActive(true);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            freeLookCamera.SetActive(false);
         }
     }
 }
