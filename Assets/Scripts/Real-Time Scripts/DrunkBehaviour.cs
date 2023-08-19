a9cdd0f463b225b4d8ee11229ec8ee73e15e8d00
JoeyPaul <joe.kallman@gmail.com>
2023-08-19 17:23:26 +1000

    Drunk (Update)
    Not fixed yet, but working on a solution where they will move on the grid.
    
    I think the current problem is agent.stopMoving(true) locks their transform.

--- Assets/Scripts/Real-Time Scripts/DrunkBehaviour.cs
+++ Assets/Scripts/Real-Time Scripts/DrunkBehaviour.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class DrunkBehaviour : MonoBehaviour
 {
+    [SerializeField] LayerMask obstacleLayer;
+
     [SerializeField] private float minWaitTime = 3f;
     [SerializeField] private float maxWaitTime = 10f;
+    [SerializeField] private float moveCooldown = 1f;
     private float elapsedTime = -1f;
+    private float timeSinceMoved = 0f;
     private float waitTime;
     public float wanderTime = 10f;
     [SerializeField] private float xLimit;
     [SerializeField] private float zLimit;
 
     Vector3 originalPosition;
+
+    bool canMove = false;
+
+    [SerializeField] int animationFrames;
+    [SerializeField] int animationLength;
 
     private NavMeshAgent agent;
     void Start()
@@ -36,6 +47,16 @@ public class DrunkBehaviour : MonoBehaviour
                 if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
                 {
                     SetRandomDestinationOrReturnToOriginalPosition();
+                    agent.isStopped = true;
+                    Vector3 destination = new Vector3(agent.destination.x, agent.destination.y, agent.destination.z);
+                    if (canMove && timeSinceMoved > moveCooldown)
+                    {
+                        StartCoroutine(MovePlayer(ConvertToClosestDirection(destination),false));
+                    }
+                    else
+                    {
+                        timeSinceMoved += Time.deltaTime;
+                    }
                 }
             }
         }
@@ -57,6 +78,41 @@ public class DrunkBehaviour : MonoBehaviour
                     elapsedTime = 0.0f;
                 }
             }
+        }
+    }
+
+    public IEnumerator MovePlayer(Vector3 moveDirection, bool completeTurn)
+    {
+        if (!Physics.Raycast(transform.position, moveDirection, 1, obstacleLayer) && canMove)
+        {
+            canMove = false;
+            Vector3 startPos = transform.position;
+            Vector3 nextPos = transform.position + moveDirection;
+          
+                print("should mve");
+                transform.position = Vector3.Lerp(startPos, nextPos,(float)animationFrames);
+                timeSinceMoved = 0.0f;
+            transform.position = nextPos;
+            canMove = true;
+        }
+        yield return new WaitForSeconds(0.1f);
+    }
+
+    public Vector3 ConvertToClosestDirection(Vector3 inputVector)
+    {
+        Vector3 normalizedVector = inputVector.normalized;
+
+        if (Mathf.Abs(normalizedVector.x) >= Mathf.Abs(normalizedVector.y) && Mathf.Abs(normalizedVector.x) >= Mathf.Abs(normalizedVector.z))
+        {
+            return new Vector3(Mathf.Sign(normalizedVector.x), 0, 0);
+        }
+        else if (Mathf.Abs(normalizedVector.y) >= Mathf.Abs(normalizedVector.x) && Mathf.Abs(normalizedVector.y) >= Mathf.Abs(normalizedVector.z))
+        {
+            return new Vector3(0, Mathf.Sign(normalizedVector.y), 0);
+        }
+        else
+        {
+            return new Vector3(0, 0, Mathf.Sign(normalizedVector.z));
         }
     }
 }
